a02f430e9afbd9cef425d428e303b104b4104f9a
Mark Lee <electronjs@lazymalevolence.com>
2020-02-24 12:13:38 -0800

    build: add documentation publishing script

---
+++ test/ci/publish_docs.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if test -z "$PUBLISH_BRANCH"; then
+    PUBLISH_BRANCH=gh-pages
+fi
+
+npm run typedoc
+
+if ! git branch --list | grep --quiet $PUBLISH_BRANCH; then
+    git checkout --orphan $PUBLISH_BRANCH
+    git rm --cached --recursive .
+    echo typedoc > .gitignore
+    touch .nojekyll
+fi
+
+mv typedoc/* .
+
+git add .
+git config user.name "$GITHUB_ACTOR"
+git config user.email "$GITHUB_ACTOR@users.noreply.github.com"
+git commit -m "Publish docs"
+# git push $PUSH_ARGS origin $PUBLISH_BRANCH
+
+git checkout -
